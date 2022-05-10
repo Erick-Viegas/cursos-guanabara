44add24ed1dc305aced45b38f97d5ff6ee89a231
Erick-Viegas <viegas.erick2005@gmail.com>
2022-05-10 18:52:47 -0300

    Atualização

---
+++ exercicios/condicoes/horarios.js
@@ -0,0 +1,15 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora <= 12 && hora >= 5) {
+    console.log('Bom dia!')
+} else if (hora < 18 && hora > 12) {
+    console.log('Boa Tarde!')
+} else if (hora >= 18 && hora <= 24) {
+    console.log('boa noite')
+} else if (hora >= 0 && hora <= 4) {
+    console.log('Boa madrugada')
+}
+  else if (hora >= 25) {
+    console.log('esse valor não existe no ciclo horário.')
+} 
