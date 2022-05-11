bccbca9c47e0c07ce28c0a621beab0efa76419b8
Erick-Viegas <viegas.erick2005@gmail.com>
2022-05-11 20:21:08 -0300

    centralizar a imagem

--- projeto-sites/script.js
+++ projeto-sites/script.js
@@ -7,33 +7,34 @@ var img = window.document.getElementById('imagem')
  var minuto = data.getMinutes()
  var segundo = data.getSeconds()
 
-msg.innerHTML = `Agora são ${hora} Horas ${minuto} minutos e ${segundo} segundos`
 
-if (hora >= 5 && hora <= 12) {
+msg.innerHTML = `Agora são ${hora} Horas ${minuto} minutos e ${segundo} segundos no horário de Brasília`
+
+if (hora >= 5 && hora <= 11) {
  img.src = 'imagens/manhã.jpg'
- document.body.style.background = '#f4ff8e'
+ document.body.style.background = '#ff8000'
 
-} else if (hora >= 13 && hora <= 18) {
+} else if (hora >= 12 && hora <= 17) {
  img.src = 'imagens/tarde.webp'
- document.body.style.background = '#ff7323'
+ document.body.style.background = '#fff71c'
  img.style.width = '375px'
 } else {
  img.src = 'imagens/noite.jpg'
  document.body.style.background = '#0b0034'
  img.style.width = '375px'
 } 
-if (hora >= 5 && hora <= 12) {
+if (hora >= 5 && hora <= 11) {
 var dia = window.document.getElementById('manha')
-dia.innerHTML = "bom dia!"
+dia.innerHTML = "Bom dia!"
 
  }
-if (hora >= 13 && hora <= 18) {
+if (hora >= 12 && hora <= 17) {
     var dia = window.document.getElementById('tarde')
-    dia.innerHTML = "boa tarde!"
+    dia.innerHTML = "Boa tarde!"
     
 }
 if (hora >= 18 && hora <= 23) {
     var dia = window.document.getElementById('noite')
-    dia.innerHTML = "boa noite!"
+    dia.innerHTML = "Boa noite!"
 }
 }
